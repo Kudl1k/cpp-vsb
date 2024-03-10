df24c5398d0c7102773a1128d0c7a45afedb090e
Kudl1k <stepanek.kudlacek@gmail.com>
2024-03-10 19:26:16 +0100

    created build

---
+++ task4/build/CMakeFiles/tasks.dir/main.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/tasks.dir/main.cpp.o: /home/kudla/cpp-vsb/task4/main.cpp \
+ /usr/include/stdc-predef.h /home/kudla/cpp-vsb/task4/tasks.h
---
+++ task4/build/CMakeFiles/tasks.dir/tasks.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/tasks.dir/tasks.cpp.o: /home/kudla/cpp-vsb/task4/tasks.cpp \
+ /usr/include/stdc-predef.h /home/kudla/cpp-vsb/task4/tasks.h
