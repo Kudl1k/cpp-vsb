e53dd68184ad2f94ae936ee858b88a05b41083b0
Kudl1k <stepanek.kudlacek@gmail.com>
2024-04-01 23:51:57 +0200

    functions in cpp

--- task7/tasks.cpp
+++ task7/tasks.cpp
@@ -19,3 +19,23 @@ Score calculate_score(Student student, Exam exam) {
     int diff = math_diff + coding_diff;
     return diff / 50;
 }
+
+std::vector<std::pair<Student, Score>> find_best_n_students(std::vector<Student> students, const Exam &exam, size_t n)
+{
+    return std::vector<std::pair<Student, Score>>();
+}
+
+size_t max_score_difference(const std::vector<Student> &students, const std::vector<Exam> &exams)
+{
+    return size_t();
+}
+
+std::pair<std::unordered_set<Student>, std::unordered_set<Student>> filter_students(const std::vector<Student> &students, const std::vector<Exam> &exams)
+{
+    return std::pair<std::unordered_set<Student>, std::unordered_set<Student>>();
+}
+
+Leaderboard get_leaderboard_of_each_subject(const std::vector<Student> &students, const std::vector<Exam> &exams)
+{
+    return Leaderboard();
+}
