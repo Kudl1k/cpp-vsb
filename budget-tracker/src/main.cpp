f7c0463d99ee1adf50e1a2e72f1b0e363874e242
Kudl1k <stepanek.kudlacek@gmail.com>
2024-04-08 21:30:29 +0200

    added new font

--- budget-tracker/src/main.cpp
+++ budget-tracker/src/main.cpp
@@ -4,7 +4,11 @@
 
 int main(int argc, char **argv) {
   QApplication a(argc,argv);
-  
+
+  QFont font("Jetbrains Mono");
+  font.setStyleHint(QFont::Monospace);
+  QApplication::setFont(font);
+    
   GUI g = new GUI(new QWidget);
 
   g.show();
